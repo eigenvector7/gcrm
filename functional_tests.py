a3a87fe9c0b90d96b25f468836d0e08e2da666e4
nish c <eigenvector7@users.noreply.github.com>
2017-06-10 22:20:05 -0700

    Functional test updated to reflect basic user interaction

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewSessionTest(unittest.TestCase):
@@ -15,15 +17,32 @@ class NewSessionTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
         # She notices the page title and header mention to-do lists
         self.assertIn('G-CRM', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('TO-DO',header_text)
+
 
         # She is invited to enter a new client information right away
+        inputbox = self.browser.find_element_by_id('id_new_client')
+
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a new client information'
+        )
+
+
+
+
 
         # She fills in the form presented with the information
-
+        inputbox.send_keys('John Doe')
         # When she hits enter, the page updates, and now the page lists
+        inputbox.send_keys(keys.Enter)
+        time.sleep(1)
         # <Client 1 Persona> <Status> <Notes> <Last Commmunication> <Next Step>
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: John Doe' for row in rows)
+        )
         # There is still a form inviting her to add another item. She enters
         # <Client 2 Persona> <Status> <Notes> <Last Commmunication> <Next Step>
 
@@ -35,6 +54,7 @@ class NewSessionTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her CRM is still there.
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     print("Hello World")
