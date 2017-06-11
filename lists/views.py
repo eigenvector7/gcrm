da9bfc89fc1be86a15b8a1894975483d9414e333
nish c <eigenvector7@users.noreply.github.com>
2017-06-10 23:00:35 -0700

    updating unit tests to use templates and adding templates directory

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>G-CRM</title></html>')
+    return render(request,"home.html")
 # Create your views here.
